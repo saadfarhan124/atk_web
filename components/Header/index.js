c046cd86d3fa98af64d53b74cd579872eb2a941c
AebadUQ <aebadulquadir123@gmail.com>
2022-10-11 12:27:11 -0700

    commitOne

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
+const Container = styled.div`
+  display: flex;
+  background-color: black;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+`;
+const ImgNews = styled.img`
+  object-fit: contain;
+  width: 20vw;
+`;
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Container>
+      <ImgNews src={"/static/header_logo.png"} />
+    </Container>
+  );
+};
 
-export default Header
+export default Header;
