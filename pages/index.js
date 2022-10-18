5a573e11718684ea0c08f4ca64898a1553c3d534
5aad <saad_anwer@live.com>
2022-10-18 14:40:38 +0500

    fix

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,9 @@ export default function Home() {
       <Scroll />
       {/* <Brand /> */}
       <Overview />
-      <div style={{height:'100vh'}}></div>
-      {/* <Agency />
-      <Footer /> */}
+      <div style={{height:'20vh'}}></div>
+      <Agency />
+      <Footer />
     </div>
   );
 }
