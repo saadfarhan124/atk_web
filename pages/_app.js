35f04c326731a01fdf6dde4a50d52c9dc9b2e3e5
5aad <saad_anwer@live.com>
2022-10-21 18:01:18 +0500

    fox

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,38 @@ import "swiper/css/free-mode";
 import "swiper/css/scrollbar";
 import { ParallaxProvider } from "react-scroll-parallax";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ParallaxProvider>
+   
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+       
+        <meta name="name" content="ATK" />
+        <meta
+          name="description"
+          content="ATK."
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="/atk.png"
+        />
+        <meta
+          property="og:description"
+          content="ATK."
+        />
+        <meta property="og:title" content="ATK" />
+        <meta property="og:url" content="https://atk.com" />
+
+
+        <title>ATK</title>
+      </Head>
       <Component {...pageProps} />
     </ParallaxProvider>
   );
